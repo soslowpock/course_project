ea941bfc9fb19467b764ea3391618ddebc4294eb
AlekseiRylshchikov <aleksei.rylshchikov@gmail.com>
2023-03-11 20:25:15 +0300

    completed 1 task

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,89 +1,105 @@
 import React, {useState} from "react";
 import API from "../api";
-// import "bootstrap/dist/css/bootstrap.css"
 
 
-
-/*
-
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-
-
-*/
-
-
-// const users = API.users.fetchAll()
-
-const TableHeader = () => {
-  return (
-    <tr>
-      <th>Имя</th>
-      <th>Качества</th>
-      <th>Профессии</th>
-      <th>Встретился, раз</th>
-      <th>Оценка</th>
-  </tr>
-  )
-}
-
-const TableRows = () => {
-  return (
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-  </tr>
-  )
-}
-
-const Table = () => {
-  return (
-    <table className="table table-dark table-striped">
-  <thead>
-   <TableHeader />
-  </thead>
-  <tbody>
-    <TableRows />
-  </tbody>
-    </table>
-  )
-
-}
 
 const Users = () => {
-  return <Table />
   
+  const userDataArray = API.users.fetchAll()
+  const [users, setUsers] = useState(userDataArray)
+
+
+  const renderUsers = (users) => {
+    return users.map((user) => renderUser(user))
   }
+
+  const renderUser = (user) => {
+    return (
+      <tr key={user._id}>
+        <th>{user.name}</th>
+        <td>{renderQualities(user)}</td>
+        <td>{user.profession.name}</td>
+        <td >{user.completedMeetings}</td>
+        <td>{user.rate}</td>
+        <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>DELETE</button></td>
+           
+         
+      </tr>
+    )
+  }
+  const renderQualities = (user) => {
+  
+    return user.qualities.map(quality => <li key={quality._id} className={getQualityClasses(quality.color)}>{quality.name}</li>)
+  }
+
+  const TableHeader = () => {
+    return (
+      <tr>
+        <th>Имя</th>
+        <th>Качества</th>
+        <th>Профессии</th>
+        <th>Встретился, раз</th>
+        <th>Оценка</th>
+        <th></th>
+      </tr>
+    )
+  }
+
+
+  const handleDelete = (id) => {
+    setUsers((prevState) => prevState.filter(user => user._id !== id))
+  }
+
+
+  const handleRefresh = () => {
+    setUsers(userDataArray)
+  }
+
+  
+  
+  const getQualityClasses = (color) => {
+    return `badge bg-${color} m-1`
+  }
+
+  const getLabelClasses = () => {
+    if (users.length === 0) {
+      return `badge bg-danger`
+    } else return `badge bg-primary`
+  }
+
+  const getLabelOutput = () => {
+    if (users.length === 1) return 'Один человек тусанёт с тобой сегодня'
+    else if (users.length >= 1 && users.length <= 4) return `${users.length} человека тусанут с тобой сегодня`
+    else if (users.length >= 5 && users.length <= 20) return `${users.length} человек тусанёт с тобой сегодня`
+    else if (users.length === 0) return `Никто не тусанёт с тобой сегодня`
+  }
+  
+
+
+    return (
+      <>
+        <h1 className={getLabelClasses()}>
+          {getLabelOutput()}
+        </h1>
+        <button
+          className="btn btn-small btn-light p-2 m-3"
+          onClick={handleRefresh}>Обновить</button>
+        <table
+          className="table table-striped table-hover">
+          <thead>
+            <TableHeader />
+          </thead>
+          <tbody>
+            {renderUsers(users)}
+          </tbody>
+        </table>
+      </>
+
+    )
+
+  }
+
+
 export default Users
 
 
