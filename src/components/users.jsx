f418f288427652f7415721baf5ef03fc055b069b
AlekseiRylshchikov <aleksei.rylshchikov@gmail.com>
2023-03-20 23:24:14 +0300

    completed 2 task

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import API from "../api";
-
+import User from "./user";
+import SearchStatus from "./searchStatus";
 
 
 const Users = () => {
@@ -9,93 +10,39 @@ const Users = () => {
   const [users, setUsers] = useState(userDataArray)
 
 
-  const renderUsers = (users) => {
-    return users.map((user) => renderUser(user))
-  }
-
-  const renderUser = (user) => {
-    return (
-      <tr key={user._id}>
-        <th>{user.name}</th>
-        <td>{renderQualities(user)}</td>
-        <td>{user.profession.name}</td>
-        <td >{user.completedMeetings}</td>
-        <td>{user.rate}</td>
-        <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>DELETE</button></td>
-           
-         
-      </tr>
-    )
-  }
-  const renderQualities = (user) => {
-  
-    return user.qualities.map(quality => <li key={quality._id} className={getQualityClasses(quality.color)}>{quality.name}</li>)
-  }
-
-  const TableHeader = () => {
-    return (
-      <tr>
-        <th>Имя</th>
-        <th>Качества</th>
-        <th>Профессии</th>
-        <th>Встретился, раз</th>
-        <th>Оценка</th>
-        <th></th>
-      </tr>
-    )
-  }
-
-
   const handleDelete = (id) => {
-    setUsers((prevState) => prevState.filter(user => user._id !== id))
+    setUsers(prevState => prevState.filter(user => user._id !== id) )
+  }
+
+  const handleBookmark = (id) => {
+    setUsers(prevState => prevState.map(user => user._id !== id ? user : { ...user, bookmark: !user.bookmark}))
   }
 
 
-  const handleRefresh = () => {
-    setUsers(userDataArray)
-  }
 
-  
-  
-  const getQualityClasses = (color) => {
-    return `badge bg-${color} m-1`
-  }
-
-  const getLabelClasses = () => {
-    if (users.length === 0) {
-      return `badge bg-danger`
-    } else return `badge bg-primary`
-  }
-
-  const getLabelOutput = () => {
-    if (users.length === 1) return 'Один человек тусанёт с тобой сегодня'
-    else if (users.length >= 1 && users.length <= 4) return `${users.length} человека тусанут с тобой сегодня`
-    else if (users.length >= 5 && users.length <= 20) return `${users.length} человек тусанёт с тобой сегодня`
-    else if (users.length === 0) return `Никто не тусанёт с тобой сегодня`
-  }
-  
-
-
-    return (
-      <>
-        <h1 className={getLabelClasses()}>
-          {getLabelOutput()}
-        </h1>
-        <button
-          className="btn btn-small btn-light p-2 m-3"
-          onClick={handleRefresh}>Обновить</button>
-        <table
-          className="table table-striped table-hover">
-          <thead>
-            <TableHeader />
-          </thead>
-          <tbody>
-            {renderUsers(users)}
-          </tbody>
-        </table>
-      </>
-
-    )
+  return (
+    <>
+    <SearchStatus usersQuantity={users.length} />
+    <table className="table">
+    <thead>
+      <tr>
+        <th scope="col">Имя</th>
+        <th scope="col">Качества</th>
+        <th scope="col">Профессия</th>
+        <th scope="col">Встретился, раз</th>
+        <th scope="col">Оценка</th>
+        <th scope="col">Избранное</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+        {users.map(user => <User key={user._id} user={{ ...user }} onDelete={handleDelete} onBookmark={handleBookmark} />)}
+      
+    </tbody>
+  </table>
+    </>
+    
+  )
 
   }
 
